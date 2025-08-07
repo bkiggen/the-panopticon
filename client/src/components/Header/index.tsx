afde496852094e8ac55635177123f1d78f69eb6a
Ben Kiggen <benkiggen@gmail.com>
2025-08-07 07:27:52 -0700

    fix import

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,10 +1,8 @@
-import panopticonLogo from "/panopticon.png";
-
 export const Header = () => {
   return (
     <header
       style={{
-        backgroundImage: `url(${panopticonLogo})`,
+        backgroundImage: `url(/panopticon.png)`,
         backgroundRepeat: "repeat-x",
         backgroundPosition: "-100px 0",
         backgroundSize: "300px auto", // maintains your 300px width
